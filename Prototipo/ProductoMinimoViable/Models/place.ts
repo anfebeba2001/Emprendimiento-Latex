d2e10f2019c7756297097342bd44417efd9efaf1
Andrés <anfebeba2001@gmail.com>
2025-05-15 17:17:29 -0500

    carousel prototype

---
+++ Prototipo/ProductoMinimoViable/Models/place.ts
@@ -0,0 +1,8 @@
+export interface place {
+    name: string;
+    description: string;
+    image: string;
+    location: string;
+    price: string;
+    rating: number;
+}
--- Prototipo/ProductoMinimoViable/src/app/pages/home/home.component.ts
+++ Prototipo/ProductoMinimoViable/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { SloganPanelComponent } from '../../../../components/Landing/slogan-panel/slogan-panel.component';
+import { CarouselComponent } from '../../../../components/carousel/carousel.component';
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [SloganPanelComponent],
+  imports: [SloganPanelComponent,CarouselComponent],
   styleUrl: './home.component.css'
 })
 export class HomeComponent {}
